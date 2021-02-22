860a3be08acd29f9846dee57dfd2ba1d12a35ee9
Sirawit Yodjit <42561981+blackjokerrr@users.noreply.github.com>
2021-02-22 17:53:58 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/hi", (req, res) => {
-  res.send("Hello world!");
+  res.send("But sometimes i want to eat coco");
 });
 
 module.exports = app;
